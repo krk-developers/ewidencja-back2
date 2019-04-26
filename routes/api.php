315688943508719096cc55923132fe606060a3d8
Artur M <artur-milkowski@tlen.pl>
2019-04-26 09:28:36 +0200

    Zabezpieczenie wszystkich zasobów autoryzacją

--- routes/api.php
+++ routes/api.php
@@ -22,33 +22,35 @@ Route::middleware('auth:api')->get(
 */
 Route::middleware('auth:api')->group(
     function () {
+        Route::apiResource('legends', 'API\LegendController')
+        ->only(['index', 'store', 'destroy']);
+    
+        Route::apiResource('events', 'API\EventController')
+            ->only(['index', 'store', 'destroy']);
+        
+        Route::apiResource('users', 'API\UserController')
+            ->only(['index']);
+        
+        Route::apiResource('user_types', 'API\TypeController')
+            ->only(['index']);
+            
+        Route::apiResource('public_holidays', 'API\PublicHolidayController')
+            ->only(['index']);
+        
+        Route::get(
+            'nearest_public_holidays',
+            'API\NearestPublicHolidayController'
+        )->name('nearest_public_holidays.index');
+        
+        Route::get(
+            'employers/{employer}/workers/{worker}',
+            'API\EmployerController@event'
+        )->name('employers.workers.event');
+        
+        Route::apiResource('employers', 'API\EmployerController')
+            ->only(['index', 'show']);
+        
+        Route::apiResource('workers', 'API\WorkerController')
+            ->only(['index', 'show']);
     }
 );
-
-Route::apiResource('legends', 'API\LegendController')
-    ->only(['index', 'store', 'destroy']);
-
-Route::apiResource('events', 'API\EventController')
-    ->only(['index', 'store', 'destroy']);
-
-Route::apiResource('users', 'API\UserController')
-    ->only(['index']);
-
-Route::apiResource('user_types', 'API\TypeController')
-    ->only(['index']);
-    
-Route::apiResource('public_holidays', 'API\PublicHolidayController')
-    ->only(['index']);
-
-Route::get(
-    'nearest_public_holidays',
-    'API\NearestPublicHolidayController'
-)->name('nearest_public_holidays.index');
-
-Route::get('employers/{employer}/workers/{worker}', 'API\EmployerController@event')
-    ->name('employers.workers.event');
-Route::apiResource('employers', 'API\EmployerController')
-    ->only(['index', 'show']);
-
-Route::apiResource('workers', 'API\WorkerController')
-    ->only(['index', 'show']);
