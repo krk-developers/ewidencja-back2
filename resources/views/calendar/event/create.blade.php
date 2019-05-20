c760a5282e039d1f66de6ccd6a0dd40c61f33e8a
Artur M <artur-milkowski@tlen.pl>
2019-05-20 10:44:11 +0200

    Zmiana kolejności pól przy dodawaniu wydarzeń

--- resources/views/calendar/event/create.blade.php
+++ resources/views/calendar/event/create.blade.php
@@ -15,15 +15,19 @@
                                 @csrf
 
                                 <div class="form-group row">
-                                    <label for="title" class="col-sm-2 col-form-label">Nazwa</label>
+                                    <label for="legend_id" class="col-sm-2 col-form-label">Legenda</label>
                                     <div class="col-sm-10">
-                                        <input type="text" id="title" name="title" class="form-control{{ $errors->has('title') ? ' is-invalid' : '' }}" value="{{ old('title') }}" placeholder="Nazwa wydarzenia"> <!-- required -->
-@if ($errors->has('title'))
+                                        <select id="legend_id" name="legend_id" class="form-control{{ $errors->has('legend_id') ? ' is-invalid' : '' }}">
+@foreach ($legends as $legend)
+                                            <option value="{{ $legend->id }}" title="{{ $legend->display_name }}">{{ $legend->name }}</option>
+@endforeach
+                                        </select>
+@if ($errors->has('legend_id'))
                                         <span class="invalid-feedback" role="alert">
-                                            <strong>{{ $errors->first('title') }}</strong>
+                                            <strong>{{ $errors->first('legend_id') }}</strong>
                                         </span>
 @else
-                                        <small id="title_help" class="form-text text-muted">Pole nieobowiązkowe</small>
+                                        <small id="legend_id_help" class="form-text text-muted">Pole obowiązkowe</small>
 @endif
                                     </div>
                                 </div>
@@ -52,24 +56,6 @@
                                         </span>
 @else
                                         <small id="end_help" class="form-text text-muted">Pole obowiązkowe</small>
-@endif
-                                    </div>
-                                </div>
-
-                                <div class="form-group row">
-                                    <label for="legend_id" class="col-sm-2 col-form-label">Legenda</label>
-                                    <div class="col-sm-10">
-                                        <select id="legend_id" name="legend_id" class="form-control{{ $errors->has('legend_id') ? ' is-invalid' : '' }}">
-@foreach ($legends as $legend)
-                                            <option value="{{ $legend->id }}" title="{{ $legend->display_name }}">{{ $legend->name }}</option>
-@endforeach
-                                        </select>
-@if ($errors->has('legend_id'))
-                                        <span class="invalid-feedback" role="alert">
-                                            <strong>{{ $errors->first('legend_id') }}</strong>
-                                        </span>
-@else
-                                        <small id="legend_id_help" class="form-text text-muted">Pole obowiązkowe</small>
 @endif
                                     </div>
                                 </div>
@@ -106,6 +92,20 @@
                                         </span>
 @else
                                         <small id="worker_id_help" class="form-text text-muted">Pole obowiązkowe</small>
+@endif
+                                    </div>
+                                </div>
+                                
+                                <div class="form-group row">
+                                    <label for="title" class="col-sm-2 col-form-label">Nazwa</label>
+                                    <div class="col-sm-10">
+                                        <input type="text" id="title" name="title" class="form-control{{ $errors->has('title') ? ' is-invalid' : '' }}" value="{{ old('title') }}" placeholder="Nazwa wydarzenia"> <!-- required -->
+@if ($errors->has('title'))
+                                        <span class="invalid-feedback" role="alert">
+                                            <strong>{{ $errors->first('title') }}</strong>
+                                        </span>
+@else
+                                        <small id="title_help" class="form-text text-muted">Pole nieobowiązkowe</small>
 @endif
                                     </div>
                                 </div>
