6efea764bb6a9f577e9e92972b3130a9945869b0
Artur M <artur-milkowski@tlen.pl>
2019-06-11 10:28:25 +0200

    Zmiana wyglądu stopki

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -2,13 +2,14 @@
         <div class="container-fluid">
             <div class="row mt-5">
                 <div class="col-sm border-top pt-2 small text-secondary">
-                        Zalogowany jako: <strong>{{ $user->name }}</strong>. 
-                        Uprawnienia: <strong>{{ $user->type->display_name }}</strong>.
+                        <strong>{{ $user->name }}</strong>. 
+                        <strong>{{ $user->type->display_name }}</strong>.
 @if (App::environment('local'))
-                        Ścieżka: <strong>{{ $path }}</strong>.
+                        {{-- Ścieżka: <strong>{{ $path }}</strong>. --}}
                         Route action: <strong>{{ Route::currentRouteAction() }}</strong>.
                         Route name: <strong>{{ Route::currentRouteName() }}</strong>.
                         Środowisko: <strong>{{ App::environment() }}</strong>.
+                        {{-- Produkcja: {{ App::isProduction() }} --}}
 @else
                         Aplikacja wykorzystuje <i class="fas fa-cookie"></i> ciastka.
 @endif
