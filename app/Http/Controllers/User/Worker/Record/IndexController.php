fc10d2c3502e5e18f2e49f467896a6536c40b70d
Artur M <artur-milkowski@tlen.pl>
2019-06-13 11:37:42 +0200

    Powrót do poprzedniej wersji obliczania ewidencji
    Ewidencja indywidualna.

--- app/Http/Controllers/User/Worker/Record/IndexController.php
+++ app/Http/Controllers/User/Worker/Record/IndexController.php
@@ -35,61 +35,39 @@ class IndexController extends Controller
     ): View {
         $admin = $request->query('admin');
         
-        $start = $this->periodStart($year_month); // Days::start($year_month);  // start period time for which we calculate the statistics
-        // dd($start);
+        $start = Days::start($year_month);  // start period time for which we calculate the statistics
 
         $monthName = $start->monthName;
-        // dd($monthName);
-        $end = $this->periodEnd($monthName, $start);  // Days::end($monthName, $start);  // current day or end of the month
-        // dd($end);
+
+        $end = Days::end($monthName, $start);  // current day or end of the month
+
         $daysInMonth = $start->daysInMonth;  // number of days in a month
-        // dd($daysInMonth);
+        
         $isFuture = $start > Carbon::now();  // whether the user calculates statistics for a future date
-        
-        // $previousMonthStart = $start->subMonth()->startOfMonth();
-        $previousMonthStartAsYearMonth = $this->previousMonthStartAsYearMonth($start);  //$previousMonthStart->format('Y-m');
-        // dd($previousMonthStartAsYearMonth);
+
+        $previousMonthStart = $start->subMonth()->startOfMonth();
+        $previousMonthStartAsYearMonth = $previousMonthStart->format('Y-m');
         $nextMonth = $start->addMonth()->format('Y-m');
-        // dd($nextMonth);
-        
+
         $timePeriod = CarbonPeriod::between($start, $end);  // the period of time for which we calculate the statistics
 
         $timePeriod = Days::weekendFilter($timePeriod);
-        
-        /*
+
         // number of public holidays in a month
         $publicHolidaysInMonth = Event::publicHolidaysInMonth(
             $start->year, $start->month
         );
         $pluckedPublicHolidaysInMonth = $publicHolidaysInMonth->pluck('start');
-        */
-        $publicHolidaysInMonth = $this->publicHolidaysInMonth($start);
-        // dd($publicHolidaysInMonth);
 
-        /*
         $timePeriodPublicHolidayFilter = Days::publicHolidayFilter(
             $timePeriod, $pluckedPublicHolidaysInMonth
         );
         $timePeriodPublicHolidayFilterCount = $timePeriodPublicHolidayFilter
             ->count();
-        */
-        $timePeriodPublicHolidayFilterCount = $this->timePeriodPublicHolidayFilterCount(
-            $timePeriod, $publicHolidaysInMonth
-        );
-        // dd($timePeriodPublicHolidayFilterCount);
-        
-        /*
-        $workerEvents = $worker->eventsByTimePeriod1(
-            (string) $start, (string) $end, $employer->id
-        );
-        $absenceInDays = Days::absenceInDays($workerEvents);
-        */
-        $absenceInDays = $this->absenceInDays($worker, $employer, $start, $end);
-        // dd($absenceInDays);
 
+        $workerEvents = $worker->eventsByTimePeriod1((string) $start, (string) $end, $employer->id);
+        $absenceInDays = Days::absenceInDays($workerEvents);
         $workingDays = $timePeriod->count() - $absenceInDays;
-        
-        // $this->calculate($worker, $employer, $year_month);
 
         return view(
             'user.worker.record.index',
